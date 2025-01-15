c18f42f9db72968b3057b9348a29c3aeaa09aaeb
baizhou <cbzxiaoyao@outlook.com>
2025-01-15 19:56:24 +0800

    Add support for list todo and add todo

--- src/main/java/Wind.java
+++ src/main/java/Wind.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Wind {
@@ -9,17 +11,21 @@ public class Wind {
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from wind\n" + logo);
         System.out.println("Hello! I'm Wind\nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
+        List<String> todos = new LinkedList<>();
         while (true) {
-            // listen for input
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < todos.size(); i++) {
+                    System.out.println(i + 1 + ". " + todos.get(i));
+                }
             } else {
-                System.out.println(input);
+                System.out.println("added: " + input);
+                todos.add(input);
             }
         }
     }
